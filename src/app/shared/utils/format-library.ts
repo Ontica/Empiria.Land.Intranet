93a6f7fe1c332c34d3be18b4145a67a6591dd1c2
Janeth Montero <janeth.montero@ontica.org>
2021-02-03 18:03:30 -0700

    Functions firstLetterToLowerCase and formatBytes added to FormatLibrary

--- src/app/shared/utils/format-library.ts
+++ src/app/shared/utils/format-library.ts
@@ -12,4 +12,29 @@ export class FormatLibrary {
     .reduce((prev, curr) => ({ ...prev, [curr[0]]: curr[1] }), {});
   }
 
+  static firstLetterToLowerCase = (s) => {
+    if (typeof s !== 'string'){
+      return '';
+    }
+    return s.charAt(0).toLowerCase() + s.slice(1);
+  }
+
+  static formatBytes(bytes, decimals = 2, binaryUnits = false): string {
+    if (bytes === 0) {
+      return '0 Bytes';
+    }
+
+    const unitMultiple = (binaryUnits) ? 1024 : 1000;
+
+    // 1000 bytes in 1 Kilobyte (KB) or 1024 bytes for the binary version (KiB)
+    const unitNames = (unitMultiple === 1024) ?
+        ['Bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'] :
+        ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+    const unitChanges = Math.floor(Math.log(bytes) / Math.log(unitMultiple));
+
+    return parseFloat((bytes / Math.pow(unitMultiple, unitChanges))
+            .toFixed(decimals || 0)) + ' ' + unitNames[unitChanges];
+  }
+
 }
