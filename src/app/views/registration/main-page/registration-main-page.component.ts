fa9632f28339d7c9ebd0b4c0d12ade4fee52a24e
José Manuel Cota <jmcota@ontica.org>
2021-03-14 22:46:09 -0400

    Rfx Update RegistrationMainPage component to use InstrumentRecording

--- src/app/views/registration/main-page/registration-main-page.component.ts
+++ src/app/views/registration/main-page/registration-main-page.component.ts
@@ -5,14 +5,23 @@
  * See LICENSE.txt in the project root for complete license information.
  */
 
-import { Component, Input, OnChanges, OnDestroy } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, ViewChild } from '@angular/core';
 
-import { isEmpty } from '@app/core';
+import { Assertion, Command, EventInfo, isEmpty } from '@app/core';
+
 import { PresentationLayer, SubscriptionHelper } from '@app/core/presentation';
 
-import { InstrumentsStateSelector } from '@app/core/presentation/presentation-types';
+import { InstrumentsRecordingCommandType, InstrumentsRecordingStateSelector } from '@app/core/presentation/presentation-types';
 
-import { Instrument, EmptyInstrument, EmptyInstrumentActions } from '@app/models';
+import { InstrumentRecording, InstrumentRecordingActions, InstrumentFields,
+         EmptyInstrumentRecording, EmptyInstrumentRecordingActions } from '@app/models';
+
+import { InstrumentEditorEventType } from '@app/views/recordable-subjects/instrument/instrument-editor.component';
+
+import {
+  FilePrintPreviewComponent
+} from '@app/shared/form-controls/file-print-preview/file-print-preview.component';
+
 
 
 @Component({
@@ -23,7 +32,10 @@ export class RegistrationMainPageComponent implements OnChanges, OnDestroy {
 
   @Input() transactionUID: string = 'Empty';
 
-  instrument: Instrument = EmptyInstrument;
+  @ViewChild('filePrintPreview', { static: true }) filePrintPreview: FilePrintPreviewComponent;
+
+  instrumentRecording: InstrumentRecording = EmptyInstrumentRecording;
+  actions: InstrumentRecordingActions = EmptyInstrumentRecordingActions;
 
   helper: SubscriptionHelper;
 
@@ -35,14 +47,16 @@ export class RegistrationMainPageComponent implements OnChanges, OnDestroy {
 
 
   ngOnChanges() {
-    this.helper.select<Instrument>(InstrumentsStateSelector.TRANSACTION_INSTRUMENT, this.transactionUID)
+    this.helper.select<InstrumentRecording>(
+                                        InstrumentsRecordingStateSelector.TRANSACTION_INSTRUMENT_RECORDING,
+                                        this.transactionUID)
       .subscribe(x => {
-        if (isEmpty(x)) {
-          this.instrument = EmptyInstrument;
-          this.instrument.actions = x.actions ?? EmptyInstrumentActions;
-        } else {
-          this.instrument = x;
-        }
+
+        console.log('ngOnChanges', x);
+
+        this.instrumentRecording = x;
+        this.actions = x.actions;
+
         this.resetPanelState();
       });
   }
@@ -55,6 +69,46 @@ export class RegistrationMainPageComponent implements OnChanges, OnDestroy {
 
   resetPanelState() {
     this.panelAddState = false;
+  }
+
+
+  onInstrumentEditorEvent(event: EventInfo) {
+    switch (event.type) {
+      case InstrumentEditorEventType.PRINT_REGISTRATION_STAMP_MEDIA:
+        this.filePrintPreview.open(this.instrumentRecording.stampMedia.url,
+                                   this.instrumentRecording.stampMedia.mediaType);
+        return;
+
+      case InstrumentEditorEventType.UPDATE_INSTRUMENT:
+        Assertion.assertValue(event.payload, 'event.payload');
+        Assertion.assertValue(event.payload.instrumentFields, 'event.payload.instruementFields');
+
+        this.updateInstrument(event.payload.instrumentFields as InstrumentFields);
+        return;
+
+      default:
+        throw Assertion.assertNoReachThisCode(`Unrecoginzed event ${event.type}.`);
+
+    }
+  }
+
+  // private methods
+
+  private updateInstrument(instrumentFields: InstrumentFields) {
+    let commandType = InstrumentsRecordingCommandType.UPDATE_RECORDED_INSTRUMENT;
+    if (isEmpty(this.instrumentRecording)) {
+      commandType = InstrumentsRecordingCommandType.CREATE_INSTRUMENT_RECORDING;
+    }
+
+    const command: Command = {
+      type: commandType,
+      payload: {
+        transactionUID: this.transactionUID,
+        instrument: instrumentFields
+      }
+    };
+
+    this.uiLayer.execute(command);
   }
 
 }
