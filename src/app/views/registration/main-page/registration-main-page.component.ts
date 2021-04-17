91909f90536c84fd06780105d45c8562ea606e8f
Janeth Montero <janeth.montero@ontica.org>
2021-04-16 23:42:09 -0700

    Use dispach() for selected instrumentRecording + Relocate recording-acts commands execution

--- src/app/views/registration/main-page/registration-main-page.component.ts
+++ src/app/views/registration/main-page/registration-main-page.component.ts
@@ -5,31 +5,36 @@
  * See LICENSE.txt in the project root for complete license information.
  */
 
-import { Component, Input, OnChanges, OnDestroy, ViewChild } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, ViewChild } from '@angular/core';
 
 import { Assertion, Command, EventInfo, isEmpty } from '@app/core';
 
 import { PresentationLayer, SubscriptionHelper } from '@app/core/presentation';
 
-import { RegistrationCommandType,
+import { RegistrationAction, RegistrationCommandType,
          RegistrationStateSelector } from '@app/core/presentation/presentation-types';
 
-import { InstrumentRecording, InstrumentRecordingActions, InstrumentFields,
-         EmptyInstrumentRecording, EmptyInstrumentRecordingActions } from '@app/models';
+import { InstrumentRecording, InstrumentFields, EmptyInstrumentRecording, RecordingActTypeGroup,
+         InstrumentRecordingActions, EmptyInstrumentRecordingActions } from '@app/models';
 
-import { InstrumentEditorEventType } from '@app/views/recordable-subjects/instrument/instrument-editor.component';
+import {
+  InstrumentEditorEventType
+} from '@app/views/recordable-subjects/instrument/instrument-editor.component';
 
 import {
   FilePrintPreviewComponent
 } from '@app/shared/form-controls/file-print-preview/file-print-preview.component';
 
+import { RecordingActCreatorEventType } from '../recording-acts/recording-act-creator.component';
+
+import { RecordingActsListEventType } from '../recording-acts/recording-acts-list.component';
 
 
 @Component({
   selector: 'emp-land-registration-main-page',
   templateUrl: './registration-main-page.component.html',
 })
-export class RegistrationMainPageComponent implements OnChanges, OnDestroy {
+export class RegistrationMainPageComponent implements OnInit, OnChanges, OnDestroy {
 
   @Input() transactionUID = 'Empty';
 
@@ -37,31 +42,40 @@ export class RegistrationMainPageComponent implements OnChanges, OnDestroy {
 
   instrumentRecording: InstrumentRecording = EmptyInstrumentRecording;
   actions: InstrumentRecordingActions = EmptyInstrumentRecordingActions;
-
   helper: SubscriptionHelper;
 
   panelAddState = false;
   submitted = false;
+
+  recordingActTypeGroupList: RecordingActTypeGroup[] = [];
 
   constructor(private uiLayer: PresentationLayer) {
     this.helper = uiLayer.createSubscriptionHelper();
   }
 
 
-  ngOnChanges() {
-    this.helper.select<InstrumentRecording>(RegistrationStateSelector.TRANSACTION_INSTRUMENT_RECORDING,
-                                            this.transactionUID)
+  ngOnInit(){
+    this.helper.select<InstrumentRecording>(RegistrationStateSelector.TRANSACTION_INSTRUMENT_RECORDING)
       .subscribe(x => {
         this.instrumentRecording = x;
         this.actions = x.actions;
-
-        this.resetPanelState();
+        if (!isEmpty(this.instrumentRecording)) {
+          this.loadData();
+          this.resetPanelState();
+        }
       });
+  }
+
+
+  ngOnChanges() {
+    this.uiLayer.dispatch(RegistrationAction.SELECT_TRANSACTION_INSTRUMENT_RECORDINGT,
+      {transactionUID: this.transactionUID});
   }
 
 
   ngOnDestroy() {
     this.helper.destroy();
+    this.uiLayer.dispatch(RegistrationAction.UNSELECT_TRANSACTION_INSTRUMENT_RECORDING);
   }
 
 
@@ -99,11 +113,59 @@ export class RegistrationMainPageComponent implements OnChanges, OnDestroy {
 
       default:
         throw Assertion.assertNoReachThisCode(`Unrecoginzed event ${event.type}.`);
-
     }
   }
 
-  // private methods
+
+  onRecordingActCreatorEvent(event: EventInfo) {
+    if (this.submitted) {
+      return;
+    }
+
+    switch (event.type as RecordingActCreatorEventType) {
+      case RecordingActCreatorEventType.APPEND_RECORDING_ACT:
+        Assertion.assertValue(event.payload.instrumentRecordingUID, 'event.payload.instrumentRecordingUID');
+        Assertion.assertValue(event.payload.registrationCommand, 'event.payload.registrationCommand');
+
+        this.executeCommand(RegistrationCommandType.APPEND_RECORDING_ACT, event.payload);
+        return;
+
+      default:
+        throw Assertion.assertNoReachThisCode(`Unrecoginzed event ${event.type}.`);
+    }
+  }
+
+
+  onRecordingActsListEvent(event: EventInfo) {
+    if (this.submitted) {
+      return;
+    }
+
+    switch (event.type as RecordingActsListEventType) {
+      case RecordingActsListEventType.SELECT_RECORDING_ACT:
+        this.uiLayer.dispatch(RegistrationAction.SELECT_RECORDING_ACT, event.payload );
+
+        return;
+
+      case RecordingActsListEventType.REMOVE_RECORDING_ACT:
+        this.executeCommand(RegistrationCommandType.REMOVE_RECORDING_ACT, event.payload);
+        return;
+
+      default:
+        throw Assertion.assertNoReachThisCode(`Unrecoginzed event ${event.type}.`);
+    }
+  }
+
+
+  private loadData(){
+    this.helper.select<RecordingActTypeGroup[]>(
+      RegistrationStateSelector.RECORDING_ACT_TYPES_LIST_FOR_INSTRUMENT,
+      { instrumentUID: this.instrumentRecording.instrument.uid })
+      .subscribe(x => {
+        this.recordingActTypeGroupList = x;
+      });
+  }
+
 
   private executeCommand<T>(commandType: any, payload?: any): Promise<T> {
 
