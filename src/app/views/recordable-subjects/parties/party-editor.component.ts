3eed3554368ea61627f6051ae1402ab6f8af9265
Janeth Montero <janeth.montero@ontica.org>
2021-04-29 22:31:17 -0700

    Rfx curpValid() and rfcValid() methods moved to validate.ts

--- src/app/views/recordable-subjects/parties/party-editor.component.ts
+++ src/app/views/recordable-subjects/parties/party-editor.component.ts
@@ -5,13 +5,13 @@
  * See LICENSE.txt in the project root for complete license information.
  */
 
-import { Component, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, Output } from '@angular/core';
 
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { Subject } from 'rxjs';
 
-import { isEmpty } from '@app/core';
+import { isEmpty, Validate } from '@app/core';
 
 import { EmptyRecordingActParty, RecordingActParty, RecordingActPartyFields,
          RecordingActPartyType } from '@app/models';
@@ -197,11 +197,11 @@ export class PartyEditorComponent implements OnInit, OnChanges {
   private validateData(partyAdd: RecordingActPartyFields) {
     let message = '';
     // TODO: return message to alert from data with format not valid and show confirm save data
-    if (partyAdd.party.curp && !curpValida(partyAdd.party.curp)) {
+    if (partyAdd.party.curp && !Validate.curpValid(partyAdd.party.curp)) {
       message = `La Curp no es valida: ${partyAdd.party.curp}. `;
     }
 
-    if (partyAdd.party.rfc && !validateRFC(partyAdd.party.rfc)) {
+    if (partyAdd.party.rfc && !Validate.rfcValid(partyAdd.party.rfc)) {
       message = `El RFC no es valido:  ${partyAdd.party.rfc}. `;
     }
 
@@ -240,53 +240,4 @@ export class PartyEditorComponent implements OnInit, OnChanges {
     });
   }
 
-}
-
-
-function curpValida(curp) {
-  const re = /^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/;
-  const validado = curp.match(re);
-  if (!validado) { // Does it match the general format?
-    return false;
-  }
-  // Validate that the check digit matches
-  function digitoVerificador(curp17) {
-    // Source https://consultas.curp.gob.mx/CurpSP/
-    const diccionario = '0123456789ABCDEFGHIJKLMNÑOPQRSTUVWXYZ';
-    let lngSuma = 0.0;
-    let lngDigito = 0.0;
-
-    for (let i = 0; i < 17; i++) {
-      lngSuma = lngSuma + diccionario.indexOf(curp17.charAt(i)) * (18 - i);
-    }
-    lngDigito = 10 - lngSuma % 10;
-    if (lngDigito === 10) {
-      return 0;
-    }
-    return lngDigito;
-  }
-
-  if (validado[2] !== digitoVerificador(validado[1])) {
-    return false;
-  }
-  return true;
-}
-
-
-function validateRFC(rfc) {
-  // regex from the SAT official site to validate RFCs
-  const patternPM = '^(([A-ZÑ&]{3})([0-9]{2})([0][13578]|[1][02])(([0][1-9]|[12][\\d])|[3][01])([A-Z0-9]{3}))|' +
-    '(([A-ZÑ&]{3})([0-9]{2})([0][13456789]|[1][012])(([0][1-9]|[12][\\d])|[3][0])([A-Z0-9]{3}))|' +
-    '(([A-ZÑ&]{3})([02468][048]|[13579][26])[0][2]([0][1-9]|[12][\\d])([A-Z0-9]{3}))|' +
-    '(([A-ZÑ&]{3})([0-9]{2})[0][2]([0][1-9]|[1][0-9]|[2][0-8])([A-Z0-9]{3}))$';
-  const patternPF = '^(([A-ZÑ&]{4})([0-9]{2})([0][13578]|[1][02])(([0][1-9]|[12][\\d])|[3][01])([A-Z0-9]{3}))|' +
-    '(([A-ZÑ&]{4})([0-9]{2})([0][13456789]|[1][012])(([0][1-9]|[12][\\d])|[3][0])([A-Z0-9]{3}))|' +
-    '(([A-ZÑ&]{4})([02468][048]|[13579][26])[0][2]([0][1-9]|[12][\\d])([A-Z0-9]{3}))|' +
-    '(([A-ZÑ&]{4})([0-9]{2})[0][2]([0][1-9]|[1][0-9]|[2][0-8])([A-Z0-9]{3}))$';
-
-  if (rfc.match(patternPM) || rfc.match(patternPF)) {
-    return true;
-  } else {
-    return false;
-  }
 }
