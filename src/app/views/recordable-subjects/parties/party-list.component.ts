224187508ca0fa1747c5e79695c466081385e866
Janeth Montero <janeth.montero@ontica.org>
2021-04-30 21:33:17 -0700

    Display and remove of secondary parties

--- src/app/views/recordable-subjects/parties/party-list.component.ts
+++ src/app/views/recordable-subjects/parties/party-list.component.ts
@@ -9,11 +9,13 @@ import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angu
 
 import { MatTableDataSource } from '@angular/material/table';
 
-import { EventInfo } from '@app/core';
+import { EventInfo, Identifiable } from '@app/core';
 
 import { RecordingActParty } from '@app/models';
 
 import { MessageBoxService } from '@app/shared/containers/message-box';
+
+import { ArrayLibrary } from '@app/shared/utils';
 
 export enum PartyListEventType {
   REMOVE_PARTY = 'PartyListComponent.Event.RemoveParty',
@@ -31,7 +33,10 @@ export class PartyListComponent implements OnInit, OnChanges {
   @Output() partyListEvent = new EventEmitter<EventInfo>();
 
   dataSource: MatTableDataSource<RecordingActParty>;
-  displayedColumns = ['name', 'typeIdentification', 'role', 'participationAmount', 'action'];
+
+  displayedColumns = ['name', 'identification', 'role', 'partAmount', 'action'];
+
+  secondaryPartiesGroupedByRoleList: any[] = [];
 
   constructor(private messageBox: MessageBoxService) { }
 
@@ -40,7 +45,31 @@ export class PartyListComponent implements OnInit, OnChanges {
 
 
   ngOnChanges() {
-    this.dataSource = new MatTableDataSource(this.partiesList);
+    this.dataSource = new MatTableDataSource(this.partiesList.filter( x =>  x.type === 'Primary'));
+    this.setSecondaryPartyGroupedList();
+  }
+
+
+  showRoleGroupByParty(roleGroup, primaryParty) {
+    return roleGroup.parties
+      .filter(secondaryParty => primaryParty.party.uid === secondaryParty.associatedWith.uid).length > 0;
+  }
+
+
+  getPartAmountText(party: RecordingActParty) {
+    switch (party.partUnit.uid) {
+      case 'Unit.Percentage':
+        return party.partAmount + '%';
+
+      case 'AreaUnit.SquareMeters':
+        return party.partAmount + ' m²';
+
+      case 'AreaUnit.Hectarea':
+        return party.partAmount + ' ' + party.partUnit.name;
+
+      default:
+        return party.partUnit.name;
+    }
   }
 
 
@@ -61,19 +90,40 @@ export class PartyListComponent implements OnInit, OnChanges {
   }
 
 
+  private setSecondaryPartyGroupedList() {
+    const secondaryRoles = new Set(this.partiesList.filter( party => party.type === 'Secondary')
+                                                   .map(party => party.role));
+
+    let secondaryRolesGrouped: Identifiable[] = [];
+    secondaryRoles.forEach(rol => {
+      secondaryRolesGrouped = [...ArrayLibrary.insertIfNotExist(secondaryRolesGrouped, rol, 'uid')];
+    });
+
+    this.secondaryPartiesGroupedByRoleList = [];
+
+    secondaryRolesGrouped.forEach(rol =>
+      this.secondaryPartiesGroupedByRoleList.push({
+        uid: rol.uid,
+        name: rol.name,
+        parties: this.partiesList.filter(party => rol.uid === party.role.uid),
+      }
+    ));
+  }
+
+
   private getConfirmMessage(party: RecordingActParty): string {
     return `
       <table style='margin: 0;'>
-        <tr><td>Nombre: </td><td><strong> ${party.party} </strong></td></tr>
+        <tr><td>Nombre: </td><td><strong> ${ party.party.fullName } </strong></td></tr>
 
-        <tr><td class='nowrap'>Participa como: </td><td><strong>
-          ${party.role.name ?? '-'}
-        </strong></td></tr>
+        <tr><td class='nowrap'>Participa como: </td><td><strong>${ party.role.name ?? '-' }</strong></td></tr>
+        ` + (
 
-        <tr><td>Titularidad: </td><td><strong>
-          ${party.type ?? '-'}
-        </strong></td></tr>
+        party.type === 'Primary' ?
+        `<tr><td>Titularidad: </td><td><strong> ${ this.getPartAmountText(party) }</strong></td></tr>` :
+        ''
 
+        ) + `
       </table>
 
      <br>¿Elimino el registro?`;
